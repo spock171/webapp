3872bc8dff9aeb4ecc2ad954a957806966f4a3e5
Robert Marcinkiewicz <robert.marcinkiewicz@capgemini.com>
2021-03-11 15:27:33 +0100

    init

--- app.js
+++ app.js
@@ -1,15 +1,27 @@
-const http = require('http');
 const express = require('express');
-const path = require('path');
 const app = express();
-app.use(express.json());
-app.use(express.static("express"));
-// default URL for website
-app.use('/', function(req,res){
-    res.sendFile(path.join(__dirname+'/express/index.html'));
-    //__dirname : It will resolve to your project folder.
-  });
-const server = http.createServer(app);
-const port = 3000;
-server.listen(port);
-console.debug('Server listening on port ' + port);
+
+const path = require('path')
+
+app.set('view engine', 'ejs')
+app.set('views', path.join(__dirname, 'views'))
+
+//body parser middleware
+app.use(express.urlencoded({
+    extended: true
+}));
+
+app.get('/',(req,res)=>{
+    res.render('home.ejs')
+})
+
+app.post('/',(req,res)=>{
+    console.log(req.body.name,req.body.stuff);
+
+    res.render('nextpage.ejs',{
+        name: req.body.name,
+        stuff:req.body.age
+    })
+})
+
+app.listen(process.env.PORT||3000,console.log('3000'))
